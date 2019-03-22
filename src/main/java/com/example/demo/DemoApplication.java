ebeb832a92e9d192419a7eb343c931091a4438e7
Stephane Nicoll <snicoll@pivotal.io>
2019-03-22 13:32:23 +0100

    Remove explicit declaration
    This commit removes the explicit `@EnableConfigurationProperties`
    declaration as `@ConfigurationProperties` types in the application are
    now scanned by default.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,9 @@ package com.example.demo;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableConfigurationProperties(DemoProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
